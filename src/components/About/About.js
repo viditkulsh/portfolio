160ed12071fa38fb0f8a05289a09c8824cb4548b
viditkulsh <91754462+viditkulsh@users.noreply.github.com>
2024-09-23 00:45:43 +0530

    Update favicon link and add margin to About section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,17 +6,20 @@ const About = () => {
     <section id="about-section" className="about-section">
       <h2 className="about-title">About Me</h2>
       <p className="about-description">
-      As a dedicated Blockchain Developer with a Bachelor's degree in Computer Applications (Hons) and a specialization in Blockchain from Bennett University, I am passionate about the transformative potential of blockchain technology. My academic journey and hands-on experience have equipped me with a robust understanding of blockchain systems and programming languages such as Python, Java, and Solidity.
-</p><br></br>
-<p className='about-description'> I have a proven track record in designing, implementing, and supporting decentralized blockchain applications and platforms. My proficiency in creating and deploying smart contracts, along with developing decentralized applications (DApps), has allowed me to contribute to innovative projects that emphasize security and reliability.
-</p>
-<br></br>
-<p className='about-description'>Beyond my technical skills, I thrive in both cross-functional team environments and individual projects. My strong business acumen and pragmatic approach enable me to align technological solutions with real-world needs effectively. I am particularly driven by the potential of blockchain to create secure, transparent, and efficient systems across various industries.
-</p><br></br>
-<p className='about-description'>I am eager to connect with like-minded professionals and explore opportunities that push the boundaries of blockchain technology. My career goal is to be at the forefront of blockchain innovation, contributing to projects that redefine how we interact with technology.
-</p><br></br>
-<p className='about-description'>Let’s connect and explore the endless possibilities of blockchain together!
-</p>
+        As a dedicated Blockchain Developer with a Bachelor's degree in Computer Applications (Hons) and a specialization in Blockchain from Bennett University, I am passionate about the transformative potential of blockchain technology. My academic journey and hands-on experience have equipped me with a robust understanding of blockchain systems and programming languages such as Python, Java, and Solidity.
+      </p>
+      <p className="about-description">
+        I have a proven track record in designing, implementing, and supporting decentralized blockchain applications and platforms. My proficiency in creating and deploying smart contracts, along with developing decentralized applications (DApps), has allowed me to contribute to innovative projects that emphasize security and reliability.
+      </p>
+      <p className="about-description">
+        Beyond my technical skills, I thrive in both cross-functional team environments and individual projects. My strong business acumen and pragmatic approach enable me to align technological solutions with real-world needs effectively. I am particularly driven by the potential of blockchain to create secure, transparent, and efficient systems across various industries.
+      </p>
+      <p className="about-description">
+        I am eager to connect with like-minded professionals and explore opportunities that push the boundaries of blockchain technology. My career goal is to be at the forefront of blockchain innovation, contributing to projects that redefine how we interact with technology.
+      </p>
+      <p className="about-description">
+        Let’s connect and explore the endless possibilities of blockchain together!
+      </p>
     </section>
   );
 };
